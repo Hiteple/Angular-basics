c38cc442e3de68098698eb2b40ef6419e111536c
hiteple <hiteplegold@gmail.com>
2020-04-13 14:41:57 -0300

    Adding bootstrap to the project and implementing the products-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {WelcomeComponent} from './home/welcome.component';
+import {ProductsListComponent} from './products/products-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/products/products-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html'
+})
+export class ProductsListComponent {
+
+}
