bfad8297c92a596d53b03c848ef6931780785ac9
hiteple <hiteplegold@gmail.com>
2020-04-13 16:13:27 -0300

    Finishing the table of products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {WelcomeComponent} from './home/welcome.component';
 import {ProductsListComponent} from './products/products-list.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ProductsListComponent} from './products/products-list.component';
     ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
